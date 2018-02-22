4b5205a9a535a8877201c5a603fb255bf011b3f9
merseen <nelsongacosta@gmail.com>
2018-02-22 10:27:44 -0800

    Se modifica connectionString para realizar scaffolding con base de datos MySQL

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -12,8 +12,6 @@ namespace ContosoUniversity.Data
         public DbSet<Course> Courses { get;set; }
         public DbSet<Enrollment> Enrollments { get;set; }
         public DbSet<Student> Students { get;set; }
+   }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.Entity<Student>().ToTable("Student");
-
-    }
 }
